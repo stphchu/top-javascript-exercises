78db5f5002c98cc1c3a4d7d4eeea988e99f387f8
Stephanie <29234000+stphchu@users.noreply.github.com>
2023-03-29 20:45:01 -0700

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start_num, end_num) {
+    if (start_num < 0 || end_num < 0 && 
+        Number.isInteger(start_num) == false || 
+        Number.isInteger(end_num) == false){
+        return "ERROR";
+    } else {
+        if (start_num > end_num){
+            let new_start = end_num;
+            end_num = start_num;
+            start_num = new_start;
+        }
+        let final_sum = 0;
+        for (let i = start_num; i <= end_num; i++){
+            final_sum += i; 
+        }
+        return final_sum;
+    }
 };
 
 // Do not edit below this line
