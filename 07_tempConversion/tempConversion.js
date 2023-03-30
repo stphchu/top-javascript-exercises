d9ea9e85eb5ecb60cffb0f622135623b04835539
Stephanie <29234000+stphchu@users.noreply.github.com>
2023-03-29 21:27:04 -0700

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f_temp) {
+  let celsius_temp = (f_temp - 32)*(5/9);
+  let rounded_cTemp = parseFloat(((celsius_temp*100)/100).toFixed(1));
+  return rounded_cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c_temp) {
+  let fahrenheit_temp = c_temp*(9/5) + 32;
+  let rounded_fTemp = parseFloat(((fahrenheit_temp*100)/100).toFixed(1));
+  return rounded_fTemp;
 };
 
 // Do not edit below this line
