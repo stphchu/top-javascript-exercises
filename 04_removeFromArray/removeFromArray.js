99a6c3aaa8eb94a029face6e09b750a468bf9905
Stephanie <29234000+stphchu@users.noreply.github.com>
2023-03-29 20:12:55 -0700

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...to_remove) {
+    for (let item_to_remove of to_remove){
+        for (let item_of_array of array){
+      	    if (item_to_remove === item_of_array){
+                let remove_index = array.indexOf(item_of_array);
+        	    array.splice(remove_index,1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
