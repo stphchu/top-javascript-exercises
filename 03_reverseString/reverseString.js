bd80772d55b04684720894c45dd33f5840826154
Stephanie <29234000+stphchu@users.noreply.github.com>
2023-03-29 18:15:49 -0700

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let string_as_array = string.split("");
+    let reversed_string = (string_as_array.reverse()).join('');
+    return reversed_string;
 };
 
 // Do not edit below this line
